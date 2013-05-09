5a55202e6ec64eb15c07b7d3476fca7ef41fe90d
JieLi2 <jieli2013@outlook.com>
2013-05-09 16:37:24 -0700

    Layout Update and File Manager update

--- DailySpending/App.xaml.cs
+++ DailySpending/App.xaml.cs
@@ -43,32 +43,40 @@ namespace DailySpending
         {
             Frame rootFrame = Window.Current.Content as Frame;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
+            // Extended Splash
+            if (args.PreviousExecutionState != ApplicationExecutionState.Running)
             {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
-
-                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
+                bool loadState = (args.PreviousExecutionState == ApplicationExecutionState.Terminated);
+                ExtendedSplash extendedSplash = new ExtendedSplash(args.SplashScreen, loadState);
+                Window.Current.Content = extendedSplash;
             }
 
-            if (rootFrame.Content == null)
-            {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                if (!rootFrame.Navigate(typeof(MainPage), args.Arguments))
-                {
-                    throw new Exception("Failed to create initial page");
-                }
-            }
+            //// Do not repeat app initialization when the Window already has content,
+            //// just ensure that the window is active
+            //if (rootFrame == null)
+            //{
+            //    // Create a Frame to act as the navigation context and navigate to the first page
+            //    rootFrame = new Frame();
+
+            //    if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+            //    {
+            //        //TODO: Load state from previously suspended application
+            //    }
+
+            //    // Place the frame in the current Window
+            //    Window.Current.Content = rootFrame;
+            //}
+
+            //if (rootFrame.Content == null)
+            //{
+            //    // When the navigation stack isn't restored navigate to the first page,
+            //    // configuring the new page by passing required information as a navigation
+            //    // parameter
+            //    if (!rootFrame.Navigate(typeof(MainPage), args.Arguments))
+            //    {
+            //        throw new Exception("Failed to create initial page");
+            //    }
+            //}
             // Ensure the current window is active
             Window.Current.Activate();
         }
---
+++ DailySpending/Controls/ExtendedSplash.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.Activation;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DailySpending
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ExtendedSplash
+    {
+        private SplashScreen splash;
+        internal Frame rootFrame;
+
+        public ExtendedSplash(SplashScreen splashscreen, bool loadState)
+        {
+            this.InitializeComponent();
+            this.rootFrame = new Frame();
+
+            this.GoToMainPage();
+        }
+
+        public async void GoToMainPage()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(0.5));
+
+            // Navigate to mainpage    
+            rootFrame.Navigate(typeof(MainPage));
+
+            // Place the frame in the current Window 
+            Window.Current.Content = rootFrame;
+        }
+
+    }
+}
